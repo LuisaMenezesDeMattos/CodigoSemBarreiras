20544c21c0f94d166c6a8b54af2d0ecbe378fba2
Luisa <luisamenezes.mattos@gmail.com>
2021-08-31 21:39:34 -0300

    CursoCodigoSemBarreiras

---
+++ src/Exercicios/Parte1/Lista1/Exercicio1.java
@@ -0,0 +1,34 @@
+package Exercicios.Parte1.Lista1;
+
+/**
+ * Parte: 1
+ * Lista: 1
+ * Exercício: 1
+ * ENUNCIADO:
+ *      "Escreva um algoritmo que armazene o valor 99 em uma variável Y e o valor 11 em uma variável Z.
+ *      A seguir (utilizando apenas atribuições entre variáveis) troque os seus conteúdos fazendo com que o valor que está em Y passe para Z e vice-versa.
+ *      Ao final, escrever os valores que ficaram armazenados nas variáveis."
+ */
+
+public class Exercicio1 extends Exercicios.Base {
+
+    public static void main(String[] args) {
+        do{
+
+            escreverTitulo("Programa que troca valores entre variáveis");
+            int y = 99;
+            int z = 11;
+            escreverLinha("Situação inicial:");
+            escreverLinha("y = " + y);
+            escreverLinha("z = " + z);
+            int aux = y;
+            y = z;
+            z = aux;
+            escreverLinha("Situação pós-troca:");
+            escreverLinha("y = " + y);
+            escreverLinha("z = " + z);
+
+        }while(repetir());
+    }
+
+}
