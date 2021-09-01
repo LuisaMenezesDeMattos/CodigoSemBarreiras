4943e30f198fa0f0a7900eb0a5ccdce5820c1e27
Luisa <luisamenezes.mattos@gmail.com>
2021-08-31 22:28:28 -0300

    CursoCodigoSemBarreiras

--- src/Exercicios/Base.java
+++ src/Exercicios/Base.java
@@ -13,31 +13,46 @@ import java.util.Scanner;
 public class Base {
 
     public static String lerString(){
-        try{
-            System.out.print("> ");
-            return new Scanner(System.in).nextLine();
-        }catch(Exception e){
-            escreverMsgErro(e.getMessage());
-            return null;
-        }
+        System.out.print("> ");
+        return new Scanner(System.in).nextLine();
     }
-    public static Integer lerInteger(){
-        try{
-            System.out.print("> ");
-            return new Scanner(System.in).nextInt();
-        }catch(Exception e){
-            escreverMsgErro(e.getMessage());
-            return null;
+    public static int lerInt(){
+        Integer leitura = null;
+        while(leitura == null){
+            try{
+                leitura = Integer.parseInt(lerString());
+            }catch(NumberFormatException e){
+                escreverMsgAtencao("Digite um número inteiro");
+            }
         }
+        return leitura;
     }
-    public static Double lerDouble(){
-        try{
-            System.out.print("> ");
-            return new Scanner(System.in).nextDouble();
-        }catch(Exception e){
-            escreverMsgErro(e.getMessage());
-            return null;
+    public static int lerIntPositivo(boolean seZeroIncluso){
+        int leitura = lerInt();
+        if(seZeroIncluso){
+            while(leitura < 0){
+                escreverMsgAtencao("Digite um número positivo ou zero");
+                leitura = lerInt();
+            }
         }
+        else{
+            while(leitura <= 0){
+                escreverMsgAtencao("Digite um número positivo");
+                leitura = lerInt();
+            }
+        }
+        return leitura;
+    }
+    public static double lerDouble(){
+        Double leitura = null;
+        while(leitura == null){
+            try{
+                leitura = Double.parseDouble(lerString());
+            }catch(NumberFormatException e){
+                escreverMsgAtencao("Digite um número inteiro");
+            }
+        }
+        return leitura;
     }
 
     public static void escreverMsgAtencao(String texto){
@@ -47,17 +62,16 @@ public class Base {
         System.out.println("ERRO: " + texto);
     }
     public static void escreverTitulo(String texto){
-        System.out.println("--- " + texto.toUpperCase() + " ---");
+        System.out.println("\n--- " + texto.toUpperCase() + " ---");
     }
     public static void escreverLinha(String texto){
         System.out.println(texto);
     }
 
-    public static Boolean repetir(){
+    public static boolean repetir(){
         escreverLinha("\nDeseja repetir o código? Enter para sim, 0 para não");
         String opcao = lerString();
-        if(opcao.equals("")){
-            escreverLinha("");
+        if(opcao.isBlank() || opcao.isEmpty()){
             return true;
         }
         escreverTitulo("Programa finalizado");
---
+++ src/Exercicios/Parte1/Lista1/Exercicio4.java
@@ -0,0 +1,47 @@
+package Exercicios.Parte1.Lista1;
+
+import java.text.DecimalFormat;
+
+/**
+ * Parte: 1
+ * Lista: 1
+ * Exercício: 4
+ * ENUNCIADO:
+ *      "Escreva um algoritmo para ler o número total de eleitores de um município, o número de votos brancos, nulos e válidos.
+ *      Calcular e escrever o percentual que cada um representa em relação ao total de eleitores."
+ */
+
+
+public class Exercicio4 extends Exercicios.Base  {
+
+    private static String calculaPorcentagem(int total, int votos){
+        double porcentagem = votos * 100.0 / total;
+        return new DecimalFormat("00.00'%'").format(porcentagem);
+    }
+
+    public static void main(String[] args) {
+        do{
+
+            escreverTitulo("Calculadora de votos de um município");
+            escreverLinha("Informe o número de eleitores deste município:");
+            int eleitores = lerIntPositivo(false);
+            int aux = eleitores;
+            escreverLinha("Dos " + aux + " possíveis, quantos votos foram brancos?");
+            int votosBrancos = lerIntPositivo(true);
+            aux -= votosBrancos;
+            escreverLinha("Dos " + aux + " restantes, quantos votos foram nulos?");
+            int votosNulos = lerIntPositivo(true);
+            aux -= votosNulos;
+            escreverLinha("Dos " + aux + " restantes, quantos votos foram válidos?");
+            int votosValidos = lerIntPositivo(true);
+            aux -= votosValidos;
+            escreverTitulo("Resultado : " + eleitores + " eleitores");
+            escreverLinha("|  Votos Brancos | " + calculaPorcentagem(eleitores, votosBrancos) + " |");
+            escreverLinha("|    Votos Nulos | " + calculaPorcentagem(eleitores, votosNulos)  + " |");
+            escreverLinha("|  Votos válidos | " + calculaPorcentagem(eleitores, votosValidos)  + " |");
+            escreverLinha("| Votos faltando | " + calculaPorcentagem(eleitores, aux)  + " |");
+
+        }while(repetir());
+    }
+
+}
