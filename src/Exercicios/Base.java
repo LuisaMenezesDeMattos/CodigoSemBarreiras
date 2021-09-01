934326d85694addfaaf0e400da5f8ef4c45526e9
Luisa <luisamenezes.mattos@gmail.com>
2021-08-31 21:26:56 -0300

    CursoCodigoSemBarreiras

--- src/Exercicios/Base.java
+++ src/Exercicios/Base.java
@@ -1,4 +1,37 @@
 package Exercicios;
 
+import java.util.Scanner;
+
+/**
+ * Parte: X
+ * Lista: X
+ * Exercício: X
+ * ENUNCIADO:
+ *      ""
+ */
+
 public class Base {
+
+    public static String lerString(){
+        try{
+            return new Scanner(System.in).nextLine();
+        }catch(Exception e){
+            escreverMsgErro(e.getMessage());
+            return null;
+        }
+    }
+
+    public static void escreverMsgAtencao(String texto){
+        System.out.println("* " + texto + "!");
+    }
+    public static void escreverMsgErro(String texto){
+        System.out.println("ERRO: " + texto);
+    }
+    public static void escreverTitulo(String texto){
+        System.out.println("--- " + texto.toUpperCase() + " ---");
+    }
+    public static void escreverLinha(String texto){
+        System.out.println(texto);
+    }
+
 }
